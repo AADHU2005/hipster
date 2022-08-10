f413c0cdf87363f66fcf0ed81f3aa12c4d1a75a3
N44 <nandhu90040@gmail.com>
2022-08-10 17:19:08 +0530

    Modified existing files

--- index.ts
+++ index.ts
@@ -1,17 +1,18 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 
 const app = new Application();
 
 // deno-lint-ignore no-explicit-any
-app.use(async (context:any, next:any) => {
+app.use(async (context: any, next: any) => {
   try {
     await context.send({
       root: `${Deno.cwd()}/`,
-      index: "home.html",
+      index: "index.html",
     });
   } catch {
     await next();
   }
 });
 
-await app.listen({ port: 8000 }).then(()=>{console.log("Deno server listening on port 8000")});
+await app.listen({ port: 8000 });
+console.log("Server started on http://localhost:8000");
