d4a1c8388fce4541d0c008722b60707a313cefa3
AADHU2005 <av.aadarsh.ixb@gmail.com>
2022-08-11 18:17:25 +0530

    added particles

--- files/hoverImage.js
+++ files/hoverImage.js
@@ -1,6 +1,5 @@
 // deno-lint-ignore-file
 const image = document.getElementById("main_logo");
-let backgroundAudios = ["monster"];
 let hipsterImages = [
   "hipster",
   "hipster2",
@@ -9,20 +8,9 @@ let hipsterImages = [
   "hipster5",
   "hipster6",
 ];
-let audioPlaying = false;
 
 image.addEventListener("mouseover", async () => {
-  let audio = new Audio(`audio/${await getRandom(backgroundAudios)}.mp3`);
-  audio.addEventListener("ended", () => {
-    audioPlaying = false;
-    console.log("Audio ended...");
-  });
   image.src = "images/" + ((await getRandom(hipsterImages)) + ".jpg");
-  if (audioPlaying == true) return;
-  await audio.play().then(() => {
-    console.log("Audio playing...");
-    audioPlaying = true;
-  });
 });
 
 async function getRandom(array) {
